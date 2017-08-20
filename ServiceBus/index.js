a62bb5336de8a9122370e817f321b274d02708e3
Stefan Sundkvist <ssundkvist@users.noreply.github.com>
2017-08-20 15:26:54 +0200

    fixed name of trigger

--- ServiceBus/index.js
+++ ServiceBus/index.js
@@ -1,5 +1,5 @@
 module.exports = function (context, timerTrigger) {
-    var message = 'Service Bus queue message created at ' + timeStamp;
+    var message = 'Service Bus queue message created at ' + timerTrigger;
     context.log(message);   
     context.bindings.topic = message;
     context.done();
